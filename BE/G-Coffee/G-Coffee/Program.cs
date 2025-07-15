a063cf957f83d4b1484976262951c1f8bd39ba81
Nguyễn Minh Thành <141386951+ScatterZero@users.noreply.github.com>
2025-07-15 21:22:40 +0700

    fix

--- BE/G-Coffee/G-Coffee/Program.cs
+++ BE/G-Coffee/G-Coffee/Program.cs
@@ -131,32 +131,7 @@ builder.Services.AddAuthorization();
 builder.Services.AddControllers();
 builder.Services.AddHttpContextAccessor();
 builder.Services.AddEndpointsApiExplorer();
-//builder.Services.AddSwaggerGen(c =>
-//{
-//    c.SwaggerDoc("v1", new OpenApiInfo { Title = "G-Coffee API", Version = "v1" });
-//    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-//    {
-//        In = ParameterLocation.Header,
-//        Description = "Please enter JWT with Bearer into field",
-//        Name = "Authorization",
-//        Type = SecuritySchemeType.ApiKey,
-//        Scheme = "Bearer"
-//    });
-//    c.AddSecurityRequirement(new OpenApiSecurityRequirement
-//    {
-//        {
-//            new OpenApiSecurityScheme
-//            {
-//                Reference = new OpenApiReference
-//                {
-//                    Type = ReferenceType.SecurityScheme,
-//                    Id = "Bearer"
-//                }
-//            },
-//            new string[] {}
-//        }
-//    });
-//});
+
 
 var app = builder.Build();
 
@@ -169,8 +144,9 @@ if (app.Environment.IsDevelopment())
 
 app.UseStaticFiles();
 // Use Endpoint
+app.UseSwagger();
+app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "G-Coffee v1"));
 
-    
 
 app.UseHttpsRedirection();
 app.UseCors("AllowFrontend"); // Sử dụng CORS với policy đã định nghĩa
