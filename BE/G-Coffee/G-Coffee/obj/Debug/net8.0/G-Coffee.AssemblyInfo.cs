a979b723b76efeb6cef76e9e3058b7db1d465fd4
Nguyễn Minh Thành <141386951+ScatterZero@users.noreply.github.com>
2025-07-28 19:40:53 +0700

    fix

--- BE/G-Coffee/G-Coffee/obj/Debug/net8.0/G-Coffee.AssemblyInfo.cs
+++ BE/G-Coffee/G-Coffee/obj/Debug/net8.0/G-Coffee.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("G-Coffee")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d703440ea22a3f61fef8a2f9cae72cd267604c1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d22875ca05986c10440ffde66cb1f2d276d5cad")]
 [assembly: System.Reflection.AssemblyProductAttribute("G-Coffee")]
 [assembly: System.Reflection.AssemblyTitleAttribute("G-Coffee")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
